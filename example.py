e4339e56a96a897b3dd1d28f186db148397f7c74
eltaco <bizopoulos.alexandros@gmail.com>
2016-09-02 03:12:02 +0300

    getting rid out of dating with a weird way

--- example.py
+++ example.py
@@ -8,46 +8,23 @@ Created on Fri Aug 19 21:40:19 2016
 import datetime
 import pandas as pd
 df = pd.read_csv('C:/Users/Alexandros/Desktop/stratego-2.csv', names=['player1', 'player2', 'result', 'tournament', 'date'])
+
 player_name_list_1 = list(df['player1'])
+player_name_list_1.pop(0)# some problems on editing made me do it. However I think it's a good exercise in such types of commands
 player_name_list_2 = list(df['player2'])
+player_name_list_2.pop(0)# the same goes here as well as the other lists
 tournament = list(df['tournament'])
+tournament.pop(0)
 date = list(df['date'])
-for day in date[1:]:
-    day = str(day)
-    day = datetime.datetime.strptime(day, "%d/%m/%Y").strftime("%Y-%m-%d")
-    
-"""above there was an error about not reading 'nan'. Apparently I forgot to add a couple of dates..."""
+date.pop(0)
 result = list(df['result'])
-player_names = set(player_name_list_1 + player_name_list_2)
-players_dict = dict()
-for player_name in player_names:
-    players_dict[player_name] = [0, 0, 0]
-
-for index, values in df.iterrows():
-    player1 = values[0]
-    player2 = values[1]
-    results = values[2]
-
-    if results == '1':
-        players_dict[player1][0] += 1
-        players_dict[player2][2] += 1
-    elif results == '0,5':
-        players_dict[player1][1] += 1
-        players_dict[player2][1] += 1
-    elif results == '0':
-        players_dict[player1][2] += 1
-        players_dict[player2][0] += 1
-
-players_and_results = sorted(players_dict.items(), key=lambda x: x[1])
-# print (players_and_results)
 question = input("Tell me a player please: ")
 question_chart = []
 for position, item in enumerate(player_name_list_1):
     if item == question:
         event = player_name_list_2[position], result[position],tournament[position], date[position]
         question_chart.append(event)
-for position, item in enumerate(player_name_list_2):
-    if item == question:
+    elif question==player_name_list_2[position]:
         if result[position] == '1':
             result[position] = '0'
         elif result[position] == '0':
@@ -57,3 +34,4 @@ for position, item in enumerate(player_name_list_2):
 else:
     print("Ready")
 print(question_chart)
+
