09d872c997b91604ad52706d24db3ada3ffffc6c
eltaco <bizopoulos.alexandros@gmail.com>
2016-09-03 23:33:30 +0300

    delete some unuseful pops

--- example.py
+++ example.py
@@ -7,16 +7,12 @@ Created on Fri Aug 19 21:40:19 2016
 """
 import datetime
 import pandas as pd
-df = pd.read_csv('C:/Users/Alexandros/Desktop/stratego-2.csv', names=['player1', 'player2', 'result', 'tournament', 'date'])
-
+df = pd.read_csv('C:/Users/Alexandros/Desktop/stratego - 3.csv', names=['player1', 'player2', 'result', 'tournament', 'date'])
+print("Please the list should be in a chronological order")
 player_name_list_1 = list(df['player1'])
-player_name_list_1.pop(0)# some problems on editing made me do it. However I think it's a good exercise in such types of commands
 player_name_list_2 = list(df['player2'])
-player_name_list_2.pop(0)# the same goes here as well as the other lists
 tournament = list(df['tournament'])
-tournament.pop(0)
 date = list(df['date'])
-date.pop(0)
 result = list(df['result'])
 question = input("Tell me a player please: ")
 question_chart = []
@@ -35,3 +31,4 @@ else:
     print("Ready")
 print(question_chart)
 
+  # for some reason I can't print Aris1970
